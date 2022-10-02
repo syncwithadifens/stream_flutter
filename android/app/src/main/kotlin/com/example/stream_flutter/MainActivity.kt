a209949f8902ae9928ef94d082ae233bcf9bac10
Afiv Dicky Efendy <dickyareda@gmail.com>
2022-10-02 19:21:04 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/stream_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.stream_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Stream<int> counterNumber() async* {
+      for (var i = 1; i <= 10; i++) {
+        await Future.delayed(const Duration(seconds: 1));
+        yield i;
+      }
+    }
+
+    // NOTE: Test
+    debugPrint('Di rebuild ulang');
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stream Builder'),
+          elevation: 0,
+          centerTitle: true,
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        ),
+        body: Center(
+          child: StreamBuilder(
+            stream: counterNumber(),
+            builder: (context, snapshot) {
+              return Text(
+                '${snapshot.data}',
+                style: const TextStyle(fontSize: 24),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
